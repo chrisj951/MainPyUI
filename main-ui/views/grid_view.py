c5f669ead5f7b6668877a0ca993509f941c9c2d3
Christopher Jacobs <chrisj951@gmail.com>
2025-05-29 15:42:28 -0700

    Remove unused imports -- need to find a plugin to do this automatically

--- main-ui/views/grid_view.py
+++ main-ui/views/grid_view.py
@@ -4,12 +4,9 @@ from devices.device import Device
 from display.font_purpose import FontPurpose
 from display.display import Display
 from display.render_mode import RenderMode
-from display.resize_type import ResizeType
 from display.y_render_option import YRenderOption
-import sdl2
 from controller.controller import Controller
 from themes.theme import Theme
-from utils.logger import PyUiLogger
 from utils.py_ui_config import PyUiConfig
 from views.grid_or_list_entry import GridOrListEntry
 from views.selection import Selection
