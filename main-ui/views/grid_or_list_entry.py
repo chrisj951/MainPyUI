011007c5168b568be5753b4c16fa0c61feb5a417
Christopher Jacobs <chrisj951@gmail.com>
2025-06-01 07:30:21 -0700

    Load description in the background on a separate thread

--- main-ui/views/grid_or_list_entry.py
+++ main-ui/views/grid_or_list_entry.py
@@ -1,9 +1,7 @@
-
-
+import threading
 from typing import Callable, TypeVar
 
 from menus.games.utils.rom_info import RomInfo
-
 
 T = TypeVar('T')  # Generic input type
 
@@ -16,12 +14,11 @@ class GridOrListEntry:
         image_path_selected=None,
         description=None,
         icon=None,
-        value : T =None,
+        value: T = None,
         image_path_searcher: Callable[[T], str] = None,
         image_path_selected_searcher: Callable[[T], str] = None,
         icon_searcher: Callable[[T], str] = None,
-        primary_text_long = None,
-
+        primary_text_long=None,
     ):        
         self.primary_text = primary_text
         self.primary_text_long = primary_text_long
@@ -31,34 +28,42 @@ class GridOrListEntry:
         self.image_path_selected_searcher = image_path_selected_searcher
         self.icon_searcher = icon_searcher
 
-        if(image_path_selected is None):
-            self.image_path_selected = image_path
-        else:
-            self.image_path_selected = image_path_selected
-
-        if(value is None): 
-            self.value = primary_text
-        else:
-            self.value = value
-
-        self.description = description
+        self.image_path_selected = image_path_selected or image_path
+        self.value = value if value is not None else primary_text
         self.icon = icon
 
+        self._description = None
+        self._description_func = None
+        self._description_event = threading.Event()
+
+        if callable(description):
+            self._description_func = description
+            threading.Thread(target=self._load_description, daemon=True).start()
+        else:
+            self._description = description
+            self._description_event.set()  # Already available
+
+    def _load_description(self):
+        try:
+            self._description = self._description_func()
+        except Exception as e:
+            self._description = f"[Error loading description: {e}]"
+        finally:
+            self._description_event.set()
+
     def __str__(self) -> str:
-        # What `print(obj)` and `str(obj)` will produce:
         return self.primary_text
 
     def __repr__(self) -> str:
-        # What you'll see in lists, debuggers, etc.
         return f"<GridOrListEntry {self.primary_text!r}>"
     
     def get_image_path(self):
-        if(self.image_path is None and self.image_path_searcher is not None):
+        if self.image_path is None and self.image_path_searcher is not None:
             return self.image_path_searcher(self.value)
         return self.image_path
     
     def get_image_path_selected(self):
-        if(self.image_path_selected is None and self.image_path_selected_searcher is not None):
+        if self.image_path_selected is None and self.image_path_selected_searcher is not None:
             return self.image_path_selected_searcher(self.value)
         return self.image_path_selected
     
@@ -66,10 +71,7 @@ class GridOrListEntry:
         return self.primary_text
     
     def get_primary_text_long(self):
-        if(self.primary_text_long is None):
-            return self.primary_text
-        else:
-            return self.primary_text_long
+        return self.primary_text_long or self.primary_text
     
     def get_value_text(self):
         return self.value_text
@@ -78,22 +80,15 @@ class GridOrListEntry:
         return self.value
     
     def get_description(self):
-        if callable(self.description):
-            self.description = self.description()
-        return self.description
+        self._description_event.wait()  # Block until description is loaded
+        return self._description
 
     def get_icon(self):
-        if(self.icon is None and self.icon_searcher is not None):
+        if self.icon is None and self.icon_searcher is not None:
             return self.icon_searcher(self.value)
         return self.icon
-    
-    def get_value(self):
-        return self.value
     
     def __eq__(self, other):
         if not isinstance(other, GridOrListEntry):
             return NotImplemented
-
-        return (
-            self.value == other.value
-        )
+        return self.value == other.value
