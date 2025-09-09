304c0864524653186e184ef23e52c19ad47fbb41
Christopher Jacobs <chrisj951@gmail.com>
2025-09-09 11:16:52 -0500

    Fix naming of recents

--- main-ui/menus/games/recents_menu.py
+++ main-ui/menus/games/recents_menu.py
@@ -23,7 +23,7 @@ class RecentsMenu(RomsMenuCommon):
                     primary_text=self._remove_extension(rom_file_name)  +" (" + self._extract_game_system(rom_info.rom_file_path)+")",
                     image_path=img_path,
                     image_path_selected=img_path,
-                    description="Favorite", 
+                    description="Recents", 
                     icon=None,
                     value=rom_info)
             )
