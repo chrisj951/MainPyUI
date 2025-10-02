48bd906e1e4e24d680688850210dbcc166e2a25d
Christopher Jacobs <chrisj951@gmail.com>
2025-10-02 17:22:19 -0500

    Attempt to get onionOS supported build going

--- onionOS/source/App/PyUI/toggle.sh
+++ onionOS/source/App/PyUI/toggle.sh
@@ -10,13 +10,13 @@ else
     echo "Enabled PyUI (created $ENABLED_FILE)"
 
     # Update Themes
-    UPDATE_DIR="/mnt/SDCARD/App/PyUI/Themes"
+    UPDATE_DIR="/mnt/SDCARD/App/PyUI/Themes/*"
     THEME_DIR="/mnt/SDCARD/Themes/"
     CHECK_FILE="$THEME_DIR/Silky by DiMo/config_750x560.json"
 
-    if [ ! -f "$CHECK_FILE" ]; then
+    if [ ! -f "/mnt/SDCARD/Themes/Silky by DiMo/config_750x560.json" ]; then
         echo "Config not found, copying update files..."
-        cp -r "$UPDATE_DIR" "$THEME_DIR/"
+        cp -r /mnt/SDCARD/App/PyUI/Themes/* /mnt/SDCARD/Themes/
         echo "Update applied."
     else
         echo "Config already exists, nothing to do."
@@ -37,36 +37,38 @@ else
         BACKUP="${RUNTIME_FILE}.bak.$(date +%s)"
         cp -p "$RUNTIME_FILE" "$BACKUP"
         echo "Backup created: $BACKUP"
+        
+awk '
+{
+    a[++n] = $0
+}
+END {
+    i = 1
+    while (i <= n) {
+        if (i + 3 <= n \
+            && a[i]   ~ /PATH=.*miyoodir/ \
+            && a[i+1] ~ /LD_LIBRARY_PATH=.*miyoodir\/lib/ \
+            && a[i+2] ~ /LD_PRELOAD=.*libpadsp\.so/ \
+            && a[i+3] ~ /\.\/MainUI.*\/dev\/null.*2>&1/ ) {
 
-        awk '
-        {
-            a[++n] = $0
+            print "    if [ -f /mnt/SDCARD/App/PyUI/enabled ]; then"
+            print "        /mnt/SDCARD/App/PyUI/launch.sh"
+            print "    else"
+            print "        PATH=\"\$miyoodir/app:\$PATH\" \\\\"
+            print "        LD_LIBRARY_PATH=\"\$miyoodir/lib:/config/lib:/lib\" \\\\"
+            print "        LD_PRELOAD=\"\$miyoodir/lib/libpadsp.so\" \\\\"
+            print "        ./MainUI > /dev/null 2>&1"
+            print "    fi"
+            i += 4
+        } else {
+            print a[i]
+            i++
         }
-        END {
-            i = 1
-            while (i <= n) {
-                if (i + 3 <= n
-                    && a[i]   ~ /PATH=.*miyoodir.*\\$/ 
-                    && a[i+1] ~ /LD_LIBRARY_PATH=.*miyoodir\/lib.*\\$/ 
-                    && a[i+2] ~ /LD_PRELOAD=.*libpadsp\.so.*\\$/ 
-                    && a[i+3] ~ /(^|[[:space:]])\.\/MainUI[[:space:]]*>[[:space:]]*\/dev\/null[[:space:]]*2>&1/ ) {
+    }
+}
+' "$RUNTIME_FILE" > "${RUNTIME_FILE}.new"
 
-                    print "    if [ -f /mnt/SDCARD/App/PyUI/enabled ]; then"
-                    print "        /mnt/SDCARD/App/PyUI/launch.sh"
-                    print "    else"
-                    print "        PATH=\"$miyoodir/app:$PATH\" \\"
-                    print "        LD_LIBRARY_PATH=\"$miyoodir/lib:/config/lib:/lib\" \\"
-                    print "        LD_PRELOAD=\"$miyoodir/lib/libpadsp.so\" \\"
-                    print "        ./MainUI > /dev/null 2>&1"
-                    print "    fi"
-                    i += 4
-                } else {
-                    print a[i]
-                    i++
-                }
-            }
-        }
-        ' "$RUNTIME_FILE" > "${RUNTIME_FILE}.new"
+
 
         if [ -s "${RUNTIME_FILE}.new" ]; then
             mv "$RUNTIME_FILE.new" "$RUNTIME_FILE"
