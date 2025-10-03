34118c717afcf04151fd8c21bf001aa2a83d593e
Christopher Jacobs <chrisj951@gmail.com>
2025-10-03 08:23:25 -0500

    Restore debug setting since we're using old SDL2 for miyoo mini

--- onionOS/source/App/PyUI/launch.sh
+++ onionOS/source/App/PyUI/launch.sh
@@ -6,6 +6,7 @@ export PYSDL2_DLL_PATH="/mnt/SDCARD/App/PyUI/libs"
 export SDL_VIDEODRIVER=mmiyoo
 export SDL_AUDIODRIVER=mmiyoo
 export EGL_VIDEODRIVER=mmiyoo
+export SDL_MMIYOO_DOUBLE_BUFFER=1
 
 rm /mnt/SDCARD/App/PyUI/run.txt
 /mnt/SDCARD/App/PyUI/python3.10/bin/MainUI /mnt/SDCARD/App/PyUI/main-ui/mainui.py -device MIYOO_MINI_FLIP -logDir "/mnt/SDCARD/App/PyUI/logs" -pyUiConfig "/mnt/SDCARD/App/PyUI/py-ui-config.json" >> /mnt/SDCARD/App/PyUI/run.txt 2>&1
