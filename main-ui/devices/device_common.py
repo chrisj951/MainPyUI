82e48dab5bb470e2fec1fef82c91d507624982d3
chrisj951 <chrisj951@gmail.com>
2026-01-21 18:09:16 -0500

    Let device override the entire wifi menu if wanted

--- main-ui/devices/device_common.py
+++ main-ui/devices/device_common.py
@@ -16,6 +16,7 @@ from devices.wifi.wifi_scanner import WiFiScanner
 from devices.wifi.wifi_status import WifiStatus
 from display.display import Display
 from display.font_purpose import FontPurpose
+from menus.settings.wifi_menu import WifiMenu
 from utils import throttle
 from utils.config_copier import ConfigCopier
 from utils.logger import PyUiLogger
@@ -508,6 +509,9 @@ class DeviceCommon(AbstractDevice):
 
     def animation_divisor(self):
         return self.get_system_config().animation_speed(1)
-    
+
+    def get_wifi_menu(self):
+        return WifiMenu()
+
     def get_new_wifi_scanner(self):
         return WiFiScanner()
